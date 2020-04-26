64f1ec21c4ab1e5599d2799e12e6b848d5ee9eaf
Marqitos <marcosarnoso@msn.com>
2020-04-26 13:54:11 +0200

    Set files for 3.1.1

--- 3.5/DISM GUI/frmProgress.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class frmProgress
-    Dim Progress As Integer
-    
-
-    Private Sub frmProgress_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Timer1.Enabled = True
-    End Sub
-
-End Class
